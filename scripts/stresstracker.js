6c05e6205c7e2bb96be7aafe7809147ccab4de26
Natalie Corwin <npc227@lehigh.edu>
2025-11-19 12:22:56 -0500

    initial stress tracker

---
+++ scripts/stresstracker.js
@@ -0,0 +1,23 @@
+function smallStressEvent() {
+    if (localStorage.getItem("StressLvl") === null) {
+        localStorage.setItem("StressLvl", 0)
+    }
+    let lvl = parseInt(localStorage.getItem("StressLvl"))
+    lvl = lvl + 1
+    console.log(lvl)
+    localStorage.clear()
+    localStorage.setItem("StressLvl", lvl)
+    return lvl
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const btnSmallStress = document.getElementById('B1')
+
+    btnSmallStress.addEventListener('click', () => {
+        let result = smallStressEvent()
+        console.log(result)
+        console.log("clicked")
+    })
+
+})
